7c10088faf52f7d1723bd9f37211bf3404944038
thelonehegelian <thelonehegelian@gmail.com>
2020-08-29 15:10:04 +0100

    Pass should withdraw

--- tests/test/deed.js
+++ tests/test/deed.js
@@ -5,4 +5,19 @@ contract("Deed", (accounts) => {
 	before(async () => {
 		deed = await Deed.deployed();
 	});
+	it("Should withdraw", async () => {
+		// get balance of beneficiary
+		const initialBalance = web3.utils.toBN(
+			await web3.eth.getBalance(accounts[1])
+		);
+
+		await new Promise((resolve) => setTimeout(resolve, 5000));
+		await deed.withdraw({ from: accounts[0] });
+
+		const finalBalance = web3.utils.toBN(
+			await web3.eth.getBalance(accounts[1])
+		);
+
+		assert.equal(finalBalance.sub(initialBalance).toNumber(), 100);
+	});
 });
